a7274ccef5990fc98b4356bb148db3ae5929f093
jose casanova <jose@nayraweb.com>
2018-08-09 10:30:47 -0300

    debug blacklist

--- config/app.php
+++ config/app.php
@@ -51,6 +51,27 @@ return [
     | your application so that it is used when running Artisan tasks.
     |
     */
+    'debug_blacklist' => [
+      '_ENV' => [
+         'APP_KEY',
+         'DB_PASSWORD',
+         'REDIS_PASSWORD',
+         'MAIL_PASSWORD',
+         'PUSHER_APP_KEY',
+         'PUSHER_APP_SECRET',
+      ],
+      '_SERVER' => [
+         'APP_KEY',
+         'DB_PASSWORD',
+         'REDIS_PASSWORD',
+         'MAIL_PASSWORD',
+         'PUSHER_APP_KEY',
+         'PUSHER_APP_SECRET',
+      ],
+      '_POST' => [
+         'password',
+      ],
+    ],
 
     'url' => env('APP_URL', 'http://localhost'),
 
